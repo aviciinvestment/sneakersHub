4d8b7e57fe0138d467930378e94c2679b2b557f3
Victory Chibueze Akunna <victorychibuakunna@gmail.com>
2025-04-24 09:22:35 +0100

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,34 @@ import "../index.css";
 
 const About = () => {
   return (
-    <div className="About">
-      <p>Sneaker Company</p>
-      <h2>Fall Limited Edition Sneakers</h2>
-      <p>
+    <div
+      style={{
+        position: "relative",
+        top: "320px",
+        width: "95%",
+        margin: "auto",
+      }}
+      className="About"
+    >
+      <p
+        style={{
+          color: "rgb(107, 104, 104)",
+          fontSize: "0.8px",
+          marginBottom: "20px",
+        }}
+      >
+        Sneaker Company
+      </p>
+      <h2 style={{ color: "rgb(18, 16, 16)", fontSize: "15px" }}>
+        Fall Limited Edition Sneakers
+      </h2>
+      <p
+        style={{
+          color: "rgb(107, 104, 104)",
+          fontSize: "0.8px",
+          marginBottom: "20px",
+        }}
+      >
         These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they’ll withstand everything the
         weather can offer.
